5a7ccb3bc61b5a9b714fc4ad3ffdbae9bfc27ac6
AnithaBoinapally <anitha.boinapally@achalasolutions.com>
2025-05-21 11:40:06 +0530

    Changes regarding variable.

--- src/main/java/com/achala/test/controller/HelloController.java
+++ src/main/java/com/achala/test/controller/HelloController.java
@@ -19,6 +19,7 @@ public class HelloController {
     public String sayHello() {
     	System.out.println("anitha...");
     	int hello_int=1;
+    	System.out.println(hello_int);
         return helloService.getGreeting();
     }
 }
