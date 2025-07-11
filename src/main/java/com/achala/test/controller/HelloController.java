2f6e3c5ecb3f6c61c398da2ac68579361b8bda84
AnithaBoinapally <anitha.boinapally@achalasolutions.com>
2025-07-11 17:00:37 +0530

    Revert "Changes regarding variable."
    This reverts commit 5a7ccb3bc61b5a9b714fc4ad3ffdbae9bfc27ac6.

--- src/main/java/com/achala/test/controller/HelloController.java
+++ src/main/java/com/achala/test/controller/HelloController.java
@@ -19,7 +19,6 @@ public class HelloController {
     public String sayHello() {
     	System.out.println("anitha...");
     	int hello_int=1;
-    	System.out.println(hello_int);
         return helloService.getGreeting();
     }
 }
